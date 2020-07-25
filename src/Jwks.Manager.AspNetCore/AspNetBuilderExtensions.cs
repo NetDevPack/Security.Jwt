f977331b1c5a76d09c5f0e9df1ea520af958eb9d
Eduardo Pires <falecom@eduardopires.net.br>
2020-07-25 02:19:30 -0300

    Jwks AspNet Service Discovery Implementation

---
+++ src/Jwks.Manager.AspNetCore/AspNetBuilderExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+
+namespace Jwks.Manager.AspNetCore
+{
+    public static class AspNetBuilderExtensions
+    {
+        public static IApplicationBuilder UseJwksManager(this IApplicationBuilder app, string jwksUri = "/jwks")
+        {
+            if(!jwksUri.StartsWith('/')) throw new ArgumentException("The Jwks URI must starts with '/'");
+
+            app.Map(new PathString(jwksUri), x => 
+                x.UseMiddleware<ServiceDiscoveryMiddleware>());
+
+            return app;
+        }
+    }
+}
