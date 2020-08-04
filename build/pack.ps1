54c65ec3bdabec3236e39f91b5376ccf645052dc
Bruno Brito <bhdebrito@gmail.com>
2020-08-03 22:25:24 -0300

    change icon

--- build/pack.ps1
+++ build/pack.ps1
@@ -2,13 +2,13 @@ param([string] $v)
 
 if (!$v)
 {
-    $version = '3.1.3-prerelease1.' + $([System.DateTime]::Now.ToString('MM-dd-HHmmss'))
+    $version = '1.0.0-prerelease1.' + $([System.DateTime]::Now.ToString('MM-dd-HHmmss'))
 }
 else{
 	$version = $v
 }
 Write-Host 'Version: ' $version 
 get-childitem * -include *.nupkg | remove-item
-dotnet build ..\src\AspNetCore.Jwks.Manager.sln
-dotnet test ..\src\AspNetCore.Jwks.Manager.sln
-dotnet pack ..\src\AspNetCore.Jwks.Manager.sln -o .\ -p:PackageVersion=$version
+dotnet build ..\src\NetDevPack.Security.JwtSigningCredentials.sln
+dotnet test ..\src\NetDevPack.Security.JwtSigningCredentials.sln
+dotnet pack ..\src\NetDevPack.Security.JwtSigningCredentials.sln -o .\ -p:PackageVersion=$version
