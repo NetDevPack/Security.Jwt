46e83877ef4eebeab1b7e38ff5357c3ca33e4fa9
Bruno Brito <bhdebrito@gmail.com>
2021-05-13 02:47:57 -0300

    checkpoint

--- src/NetDevPack.Security.Jwt.Store.DataProtection/AspNetCoreDataProtection.cs
+++ src/NetDevPack.Security.Jwt.Store.DataProtection/AspNetCoreDataProtection.cs
@@ -1,7 +1,10 @@
 ﻿using Microsoft.AspNetCore.DataProtection.KeyManagement;
 using Microsoft.AspNetCore.DataProtection.KeyManagement.Internal;
 using Microsoft.AspNetCore.DataProtection.Repositories;
+using Microsoft.AspNetCore.DataProtection.XmlEncryption;
+using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Microsoft.Win32;
 using NetDevPack.Security.JwtSigningCredentials;
 using NetDevPack.Security.JwtSigningCredentials.Interfaces;
 using NetDevPack.Security.JwtSigningCredentials.Model;
@@ -9,6 +12,7 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Xml.Linq;
 using System.Xml.Serialization;
@@ -18,13 +22,16 @@ namespace NetDevPack.Security.Jwt.Store.DataProtection
     public class AspNetCoreDataProtection : IJsonWebKeyStore
     {
         private readonly IOptions<KeyManagementOptions> _keyManagementOptions;
+        private readonly ILoggerFactory _loggerFactory;
         private readonly IConfigureOptions<KeyManagementOptions> _options;
         private readonly IInternalXmlKeyManager _internalXmlKeyManager;
         private readonly IXmlRepository _xmlRepository;
         private const string Name = "NetDevPack.Security.Jwt";
-        public AspNetCoreDataProtection(IOptions<KeyManagementOptions> keyManagementOptions)
+        public AspNetCoreDataProtection(IOptions<KeyManagementOptions> keyManagementOptions,
+            ILoggerFactory loggerFactory)
         {
             _keyManagementOptions = keyManagementOptions;
+            _loggerFactory = loggerFactory;
             // Force it to configure xml repository.
         }
         public void Save(SecurityKeyWithPrivate securityParamteres)
@@ -92,7 +99,44 @@ namespace NetDevPack.Security.Jwt.Store.DataProtection
 
 
 
-
+        internal KeyValuePair<IXmlRepository, IXmlEncryptor> GetFallbackKeyRepositoryEncryptorPair()
+        {
+            IXmlEncryptor xmlEncryptor = (IXmlEncryptor)null;
+            DirectoryInfo forAzureWebSites = DefaultKeyStorageDirectories.Instance.GetKeyStorageDirectoryForAzureWebSites();
+            IXmlRepository key;
+            if (forAzureWebSites != null)
+            {
+                key = (IXmlRepository)new FileSystemXmlRepository(forAzureWebSites, this._loggerFactory);
+            }
+            else
+            {
+                DirectoryInfo storageDirectory = DefaultKeyStorageDirectories.Instance.GetKeyStorageDirectory();
+                if (storageDirectory != null)
+                {
+                    if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+                        xmlEncryptor = (IXmlEncryptor)new DpapiXmlEncryptor(true, this._loggerFactory);
+                    key = (IXmlRepository)new FileSystemXmlRepository(storageDirectory, this._loggerFactory);
+                }
+                else
+                {
+                    RegistryKey registryKey = (RegistryKey)null;
+                    if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+                        registryKey = RegistryXmlRepository.DefaultRegistryKey;
+                    if (registryKey != null)
+                    {
+                        RegistryKey defaultRegistryKey = RegistryXmlRepository.DefaultRegistryKey;
+                        xmlEncryptor = (IXmlEncryptor)new DpapiXmlEncryptor(true, this._loggerFactory);
+                        key = (IXmlRepository)new RegistryXmlRepository(defaultRegistryKey, this._loggerFactory);
+                    }
+                    else
+                    {
+                        throw new Exception(
+                            "Is not possible to determine which folder are the protection keys. NetDevPack.Security.JwtSigningCredentials.Store.FileSystem or NetDevPack.Security.JwtSigningCredentials.Store.EntityFrameworkCore");
+                    }
+                }
+            }
+            return new KeyValuePair<IXmlRepository, IXmlEncryptor>(key, xmlEncryptor);
+        }
 
 
     }
