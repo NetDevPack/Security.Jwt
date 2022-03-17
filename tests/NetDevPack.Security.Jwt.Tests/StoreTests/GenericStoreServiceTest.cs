c4a3cc05db2b695b72ece0219b387872a7111c05
Bruno <bruno_hbrito@hotmail.com>
2022-03-17 04:41:51 -0300

    fix: publish

--- tests/NetDevPack.Security.Jwt.Tests/StoreTests/GenericStoreServiceTest.cs
+++ tests/NetDevPack.Security.Jwt.Tests/StoreTests/GenericStoreServiceTest.cs
@@ -16,7 +16,6 @@ using NetDevPack.Security.Jwt.Tests.Warmups;
 using Xunit;
 
 namespace NetDevPack.Security.Jwt.Tests.StoreTests;
-[CollectionDefinition("Non-Parallel Collection", DisableParallelization = false)]
 public abstract class GenericStoreServiceTest<TWarmup> : IClassFixture<TWarmup>
     where TWarmup : class, IWarmupTest
 {
